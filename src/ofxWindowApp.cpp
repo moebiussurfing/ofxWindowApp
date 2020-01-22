809ce73c8534353bb1ef18af9796f23645ade6d1
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-01-22 19:24:41 -0300

    Update ofxWindowApp.cpp

--- src/ofxWindowApp.cpp
+++ src/ofxWindowApp.cpp
@@ -3,48 +3,51 @@
 //--------------------------------------------------------------
 ofxWindowApp::ofxWindowApp()
 {
-    if (autoSaveLoad)
-        loadWindow();
+	if (autoSaveLoad)
+		loadWindow();
+
+	//auto call setup
+	//setup();
 }
 
 //--------------------------------------------------------------
 ofxWindowApp::~ofxWindowApp()
 {
-    if (autoSaveLoad)
-        saveWindow();
+	if (autoSaveLoad)
+		saveWindow();
 }
 
 //--------------------------------------------------------------
 void ofxWindowApp::setup()
 {
-    if (autoSaveLoad)
-        loadWindow();
+	if (autoSaveLoad)
+		loadWindow();
 }
 
 //--------------------------------------------------------------
 void ofxWindowApp::saveWindow()
 {
-    //save window settings
-    ofWindowSettings AppWindow;
-    AppWindow.setPosition(glm::vec2(ofGetWindowPositionX(), ofGetWindowPositionY()));
-    AppWindow.setSize(ofGetWindowSize().x, ofGetWindowSize().y);
-    AppWindow.windowMode = ofGetCurrentWindow()->getWindowMode();
+	//save window settings
+	ofWindowSettings AppWindow;
+	AppWindow.setPosition(glm::vec2(ofGetWindowPositionX(), ofGetWindowPositionY()));
+	AppWindow.setSize(ofGetWindowSize().x, ofGetWindowSize().y);
+	AppWindow.windowMode = ofGetCurrentWindow()->getWindowMode();
 
-    ofJson j;
-    to_json(j, AppWindow);
-    ofSavePrettyJson(path_folder + path_filename, j);
+	ofJson j;
+	to_json(j, AppWindow);
+	ofSavePrettyJson(path_folder + path_filename, j);
 
-    ofLogNotice("ofxWindowApp") << "saveWindow: " << path_folder + path_filename;
+	ofLogNotice("ofxWindowApp") << "saveWindow: " << path_folder + path_filename;
 }
 
 //--------------------------------------------------------------
 void ofxWindowApp::loadWindow()
 {
-    //load window settings
-    ofJson j = ofLoadJson(path_folder + path_filename);
-    ofx::Serializer::ApplyWindowSettings(j);
+	//load window settings
+	ofJson j = ofLoadJson(path_folder + path_filename);
+	ofx::Serializer::ApplyWindowSettings(j);
 
-    ofLogNotice("ofxWindowApp") << "loadWindow: " << path_folder + path_filename;
+	ofLogNotice("ofxWindowApp") << "loadWindow: " << path_folder + path_filename;
 }
 
 
