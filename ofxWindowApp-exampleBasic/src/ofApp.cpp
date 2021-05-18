95ce6ead7638e7eadff02675892d37201ad05643
moebiussurfing <moebiussurfing@gmail.com>
2021-05-17 23:27:10 -0300

    Update ofApp.cpp

--- ofxWindowApp-exampleBasic/src/ofApp.cpp
+++ ofxWindowApp-exampleBasic/src/ofApp.cpp
@@ -5,7 +5,7 @@ void ofApp::setup() {
 	// required when addon started for first time on your project
 	// or when no json file settings is present into /bin/data/ofxWindowApp/ofxWindowApp.json
 	// or if you want hardcode the settings
-	windowApp.setFrameRate(60);
+	//windowApp.setFrameRate(60);
 	//windowApp.setVerticalSync(true);
 	//WindowApp.setPositionDebugInfo(ofxWindowApp::DEBUG_POSITION_TOP);
 }
